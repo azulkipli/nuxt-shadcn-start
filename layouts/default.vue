20ffd9da238ae92169e4d7ad371317b56fa7948d
azulkipli <azulkipli@gmail.com>
2025-05-27 17:00:56 +0700

    layout & page

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="defLayout">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="pageHome">
+    <h1>HOME</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  title: "Home",
+  description: "Home page NuxtJS Shadcn UI",
+  layout: "default",
+})
+
+defineSeoMeta({
+  keywords: ["nuxt", "shadcn", "starter"],
+  ogTitle: "Home | NuxtJS Shadcn UI",
+  ogDescription: "NuxtJS Shadcn UI",
+  ogImage: "https://unocss.dev/logo.svg",
+  ogUrl: "http://nuxt-shadcn-start.vercel.app",
+  ogType: "website",
+  ogDescription: "Home page NuxtJS Shadcn UI",
+  ogImage: "URL_ADDRESSxtseo.com/og-image.jpg",
+  twitterCard: "summary_large_image",
+  twitterImage: "URL_ADDRESSxtseo.com/twitter-image.jpg",
+  twitterTitle: "Home",
+  twitterDescription: "Home page NuxtJS Shadcn UI",
+})
+</script>
+
+<style>
+
+</style>
