fb02190c44e037bce582b8cdd0cc4b799002fcb4
azulkipli <azulkipli@gmail.com>
2025-05-28 08:50:49 +0700

    add useHead

--- app.vue
+++ app.vue
@@ -4,6 +4,20 @@
   </NuxtLayout>
 </template>
 <script lang="ts" setup>
+const isDev = import.meta.dev;
+useHead({
+  htmlAttrs: {
+    lang: "en",
+    class: "no-scrollbar",
+  },
+  bodyAttrs: {
+    class: isDev ? "debug-screens no-scrollbar" : "no-scrollbar",
+  },
+  titleTemplate: (param) => {
+    return param ? `${param} | NuxtShadcn` : "";
+  },
+  // link: [],
+});
 useSeoMeta({
   keywords: ["nuxt", "shadcn", "starter"],
   ogTitle: "Home | NuxtJS Shadcn UI",
@@ -17,5 +31,19 @@ useSeoMeta({
   twitterImage: "URL_ADDRESSxtseo.com/twitter-image.jpg",
   twitterTitle: "Home",
   twitterDescription: "Home page NuxtJS Shadcn UI",
-})
+});
 </script>
+<style>
+/* Hide scrollbar for Chrome, Safari and Opera */
+.no-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.no-scrollbar {
+  -ms-overflow-style: none;
+  /* IE and Edge */
+  scrollbar-width: none;
+  /* Firefox */
+}
+</style>
